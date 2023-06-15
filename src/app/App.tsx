a544eefaa4853033e817b809e6eef8cf4de38293
kykp <kykp@yandex.ru>
2023-06-15 21:18:57 +0400

    [dev] created i18n translator + langSwitcher added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,24 @@
-import {Link, Route, Routes} from "react-router-dom"
 import "./styles/index.scss"
 import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
 import {Sidebar} from "widgets/Sidebar";
-
+import {Suspense} from "react";
 
 export const App = () => {
     const {theme} = useTheme()
 
+
     return (
     <div className={classNames('app',{},[theme])}>
-        <Navbar/>
-        <div className="content-page">
-            <Sidebar/>
-            <AppRouter/>
-        </div>
+        <Suspense fallback="">
+            <Navbar/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
+        </Suspense>
 
     </div>
   )
--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -22,7 +22,7 @@ export const AppLink: FC<AppLinkProps> = (props) => {
     return (
         <Link
             to={to}
-            className={classNames(cls.AppLink, {}, [className,cls[theme]])}
+            className={classNames(cls.AppLink, {}, [className, cls[theme]])}
             {...otherProps}
         >
             {children}
