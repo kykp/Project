a90cd4c84c4c21b624b1ecdcfa98f4cc8c64e732
kykp <kykp@yandex.ru>
2023-06-21 21:43:29 +0400

    plugin fo eslint fix + scss lint

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:i18next/recommended'
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+        "i18next"
+    ],
+    rules: {
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        'indent': [2,4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        "react/react-in-jsx-scope": 0,
+        "react/jsx-uses-react": 0,
+        "import/no-unresolved": 0,
+        "import/prefer-default-export": 0,
+        "no-unused-vars": 1,
+        "react/require-default-props": 0,
+        'react/jsx-props-no-spreading': 1,
+        'react/function-component-definition': 0,
+        "no-shadow": "off",
+        "import/extensions": "off",
+        "i18next/no-literal-string": ['error', {markupOnly: true}]
+    },
+    globals: {
+        "__IS_DEV__": true,
+        "__dirname": true
+    }
+};
