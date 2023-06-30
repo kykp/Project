f84267cdb63cb837b30c7872b6fb6ee5f556e408
kykp <kykp@yandex.ru>
2023-06-30 20:54:20 +0400

    add bundleAnalyze + ErrorBoundary

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
         "no-shadow": "off",
         "import/extensions": "off",
         "i18next/no-literal-string": ['error', {markupOnly: true}],
-        "max-len": ['error', {"ignoreComments": true}]
+        "max-len": ['error', {"ignoreComments": true, code: 100}]
     },
     globals: {
         "__IS_DEV__": true,
